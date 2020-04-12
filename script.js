a5d93fad2a18c155d77c8e9ef524bab87d5e32b7
unknown <yassboudra@gmail.com>
2020-04-12 16:51:09 +0200

    improve eqation input modifier and input selector

--- script.js
+++ script.js
@@ -2,83 +2,144 @@ $(document).ready(function(){
 	
 	let input = [];
 	let num = [];
-	let inMod;
-	let inputOn = false;
+	let dataCount;
+	let data = [];
+	let equation = [];
+	let anwser = 0;
+	let mod=false;
+	let modOn = false;
+	let numMod = false;
+	let modCount = 0;
+	let left;
 	
 	const calculator = {
 		reset(){
-			for(let i = 0; i<input.length;i++){
-				input[i] = input[i].replace('[', '');
-				input[i] = input[i].replace(']', '');
+			for(let i = 0; i<data.length;i++){
+				data[i]=data[i].replace("[", "");
+				data[i]=data[i].replace("]", "");
 			}
+			anwser=0;
+			data=[];
 		},
-		selectNum(){
-			input[inMod] = '[' + input[inMod] + ']';
-			inMod--;
-			if(inMod < 0){
-				this.initInput();
+		selectLeft(key){
+			if(modOn===false){
+				this.initInput(key);
 			}
-		},
-		initInput(){
-			inputOn = true;
-			inMod = input.length-1;
-		},
-		showInput(key){
-			if(key === 'In' || key === 'L'){
-				$('#screen').html(input);
-			}else if(key != '=' && key != 'In' && key != 'L' && inputOn === false){
-				$('#screen').append(key);
-			}else if(key != '=' && key != 'In' && key != 'L' && inputOn === true){
-				$('#screen').html(input);
-			}else {
-				$('#screen').html(this.result(input));
+			data[dataCount] = '[' + data[dataCount] + ']';
+			console.log(dataCount);
+			dataCount--;
+			if(dataCount < 0){
+				dataCount = data.length-1;
 			}
+			mod=true;
+			modCount=0;
+			numMod=false;
+			$('#screen').html(data);
+		},
+		initInput(key){
+			if(key==='L'){
+				dataCount = data.length-1;
+			}else if(key==='R'){
+				dataCount = 0;
+			}
+			modOn = true;
+			mod=false;
+			numMod=false;
+			modCount=0;
+			$('#screen').html(equation);
 		},
 		saveInput(key){
-			 if(key === '+' || key === '-' || key === '/' || key === '*'){
-				 input.push(key);
-			 }else if(key != "="){
-				 input.push(key);
-			 }
+			if(key === '+' || key === '-' || key === '/' || key === '*'){
+				input.push(key);
+			}else if(key != "="){
+				input.push(key);
+			}
+			$('#screen').append(key);
 		},
-		result(input){
-			let anwser = 0;
-			for(let i = 0; i < input.length; i++){
-				let val = Number(input[i+1]);
-				if(i === 0){
-					anwser =Number(input[i]);
+		correctNum(){
+			let count = 0;
+			for(let i=0; i<input.length;i++){
+				if(input[i]!="+"){
+					data[count]+=input[i];
 				}
-				switch(input[i]){
+				if(input[i]==="+"){
+					data.push('+');
+					count=count+2;
+				}
+			}
+		},
+		result(){
+			for(let i=0;i<data.length;i++){
+				if(i === 0){
+					anwser += Number(data[i]);
+				}
+				switch(data[i]){
 					case '+':
-						anwser += val;
-					break;     
-					case '-':     
-						anwser -= val;
-					break;      
-					case '/':    
-						anwser /= val;
-					break;       
-					case '*':    
-						anwser *= val;
+						anwser += Number(data[i+1]);
 					break;
 				}
 			}
-			return anwser;
+			$('#screen').html(anwser);
+			data=[];
+			mod=false;
+			numMod=false;
+			modCount=0;
+		},
+		cleanupUndefined(){
+			for(let i=0; i<data.length;i++){
+				data[i]=data[i].replace("undefined", "");
+			}			
+		},
+		modifyInput(key){
+			let newCount = dataCount+1;
+			let countDataOp = 0;
+			let countInOp = 0;
+			let start=0;
+			if(newCount===data.length){
+				newCount=0;
+			}
+			for(let i=0;i<newCount;i++){
+				if(data[i]==="+"){
+					countDataOp++;
+				}
+			}
+			for(let i=0;i<input.length;i++){
+				if(input[i]==="+"){
+					countInOp++;
+				}
+				if(countDataOp==countInOp){
+					start=i;
+					start=start-data[newCount].length+1;
+				}
+			}
+			if(numMod===false){
+				input.splice(start,data[newCount].length,key);
+				numMod=true;
+			}else{
+				modCount++;
+				input.splice(start+modCount,0,key);
+			}
+			$('#screen').html(input);
 		}
-	};
+	}
 	
 	//get value of key in string format
 	$('.c-keys').click(function(){
 		const key = this.id.replace('c-key', '');
+		calculator.correctNum();
+		calculator.cleanupUndefined();
+		equation = data;
 		if(key === 'In'){
-			calculator.initInput();
+			calculator.initInput(key);
 		}else if(key === 'L'){
-			calculator.selectNum();
+			calculator.selectLeft(key);
+		}else if(key === '='){
+			calculator.result();
+		}else if(mod === true){
+			calculator.modifyInput(key);
 		}else{
 			calculator.saveInput(key);
-			calculator.result(key);
 		}
-		calculator.showInput(key);
 		calculator.reset();
 	});
 	
